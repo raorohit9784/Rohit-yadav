0b447d0aece417e47830486d2ac31ad7e1982577
raorohit9784 <rohitrao1455@gmail.com>
2025-08-29 10:31:52 +0530

    Add files via upload

---
+++ strings1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class strings1{
+    public static void main(String args[]) {
+        // concatenation
+        String firstname = "tony";
+        String lastname = "strack";
+        String fullname = firstname + lastname;
+        System.out.println(fullname);
+
+    }
+}
+          
+  
+    
